df2fff6b75e865191f01b3ef13cf8d460ae4412e
Wasabi95 <ancarsa.wasabi.12@gmail.com>
2024-03-24 11:00:00 -0500

    added auth

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,10 @@
-// import React from "react";
-// import "@aws-amplify/ui-react/styles.css";
-// import { withAuthenticator } from "@aws-amplify/ui-react";
-
-// import Dashboard from "../Dashboard"; 
-
-// const App = ( { signOut } ) => {
-//   return (
-//     <div>
-     
-//       <Dashboard /> 
-      
-//     </div>
-//   );
-// };
-
-// export default withAuthenticator(App);
-
 import React from "react";
-
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 import Dashboard from "../Dashboard"; 
 
-const App = (  ) => {
+const App = ( { signOut } ) => {
   return (
     <div>
      
@@ -31,4 +14,29 @@ const App = (  ) => {
   );
 };
 
-export default (App);
+export default withAuthenticator(App);
+
+
+// import "@aws-amplify/ui-react/styles.css";
+// import {
+//   withAuthenticator,
+//   Button,
+//   Heading,
+//   Image,
+//   View,
+//   Card,
+// } from "@aws-amplify/ui-react";
+
+// function App({ signOut }) {
+//   return (
+//     <View className="App">
+//       <Card>
+        
+//         <Heading level={1}>We now have Auth!</Heading>
+//       </Card>
+//       <Button onClick={signOut}>Sign Out</Button>
+//     </View>
+//   );
+// }
+
+// export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css"
 import App from './components/App';
-// import { Amplify } from 'aws-amplify';
-// import config from './aws-exports';
-// Amplify.configure(config);
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 
 
